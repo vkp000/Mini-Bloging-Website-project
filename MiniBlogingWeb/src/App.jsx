ec4703fb5d1e4c07330356b6442a29ba1c10cf77
vivek <vivekprajapatvkp0@gmail.com>
2025-05-02 10:22:25 +0530

    setUp done

---
+++ MiniBlogingWeb/src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import './App.css'
+import authService from './appwrite/auth';
+import {login, logout} from "./store/authSlice"; 
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  },[])
+
+return !loading ? (
+  <div className='min-h-sc flex flew-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header />
+      <main>
+        {/* <Outlet/> */}
+      </main>
+      <Footer />
+    </div>
+  </div>
+) : (null)
+}
+
+export default App
